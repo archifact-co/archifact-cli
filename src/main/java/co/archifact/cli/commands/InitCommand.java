6c971db0e4ab8c93972d05d18595db7ad5ba2210
Andrey <andreygfranca@gmail.com>
2024-02-17 15:37:21 -0300

    feat(cli-core): invoke core services

--- src/main/java/co/archifact/cli/commands/InitCommand.java
+++ src/main/java/co/archifact/cli/commands/InitCommand.java
@@ -1,5 +1,9 @@
 package co.archifact.cli.commands;
 
+import co.archifact.core.DeployResourcesRequest;
+import co.archifact.core.ResourceManagementServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -13,5 +17,18 @@ public class InitCommand implements Runnable {
     public void run() {
         System.out.printf("Initializing new 'archifact' project: %s%n", projectName);
         // Implementation for initializing a new project
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+                .usePlaintext()
+                .build();
+
+
+        ResourceManagementServiceGrpc.ResourceManagementServiceBlockingStub serviceBlockingStub
+                = ResourceManagementServiceGrpc.newBlockingStub(channel);
+
+        serviceBlockingStub.deployResources(
+                DeployResourcesRequest.newBuilder()
+                        .build()
+        );
+        channel.shutdown();
     }
 }
